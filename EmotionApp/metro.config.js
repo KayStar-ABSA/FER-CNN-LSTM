ecef08be804914f30caf5b3cff1f766629249699
KAYSTAR\thuan <thuanpt182.work@gmail.com>
2025-06-18 21:07:33 +0700

    init

--- EmotionApp/metro.config.js
+++ EmotionApp/metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,22 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true, // giúp Metro bundler nhanh hơn
+      },
+    }),
+  },
+  // Giới hạn số lượng luồng worker nếu máy yếu hoặc hay treo
+  maxWorkers: 1,
+
+  // Tránh theo dõi những thư mục nặng gây lỗi
+  watchFolders: [
+    path.resolve(__dirname),
+  ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
