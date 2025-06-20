7545461c6b6068b0d0ebbc8ec703dc60562474c9
sonnv1912 <sonnv1912@gmail.com>
2025-06-20 11:41:56 +0700

    update: enhance app configuration and integrate camera features

--- App/app/(tabs)/index.tsx
+++ App/app/(tabs)/index.tsx
@@ -1,75 +1,108 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useCallback, useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  Camera,
+  Frame,
+  useCameraDevice,
+  useCameraPermission,
+  useFrameProcessor,
+} from "react-native-vision-camera";
+import {
+  Face,
+  FaceDetectionOptions,
+  useFaceDetector,
+} from "react-native-vision-camera-face-detector";
+import { Worklets } from "react-native-worklets-core";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function App() {
+  const faceDetectionOptions = useRef<FaceDetectionOptions>({
+    performanceMode: "accurate",
+    minFaceSize: 0.3,
+  }).current;
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const device = useCameraDevice("front");
+  const [active, setActive] = useState(false);
+  const { detectFaces } = useFaceDetector(faceDetectionOptions);
+  const [faces, setFaces] = useState<Face[]>([]);
+  const insets = useSafeAreaInsets();
 
-export default function HomeScreen() {
+  const renderFaceBoxes = useCallback((_faces: Face[], frame: Frame) => {
+    setFaces(_faces);
+
+    // const buffer = frame.toArrayBuffer()
+    // const data = new Uint8Array(buffer)
+
+    // setActive(false);
+  }, []);
+
+  const handleDetectedFacesJS = Worklets.createRunOnJS(renderFaceBoxes);
+
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      "worklet";
+
+      const objects = detectFaces(frame);
+
+      handleDetectedFacesJS(objects, frame);
+    },
+    [handleDetectedFacesJS]
+  );
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission, requestPermission]);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={{ flex: 1, marginTop: insets.top }}>
+      <TouchableOpacity
+        onPress={() => setActive(!active)}
+        style={{
+          padding: 20,
+          position: "absolute",
+          top: 0,
+          right: 0,
+          backgroundColor: "red",
+          zIndex: 1,
+        }}
+      >
+        <Text>Active</Text>
+      </TouchableOpacity>
+
+      {!!device && active ? (
+        <Camera
+          isActive={active}
+          style={StyleSheet.absoluteFill}
+          device={device}
+          frameProcessor={frameProcessor}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      ) : (
+        <Text>No Device</Text>
+      )}
+      {faces.map((face, index) => {
+        const { bounds, pitchAngle = 0, rollAngle = 0 } = face;
+        // Heuristic adjustment based on face rotation
+        const angleAdjustment = Math.abs(pitchAngle) + Math.abs(rollAngle);
+        const padding = angleAdjustment;
+
+        return (
+          <View
+            key={index}
+            style={{
+              position: "absolute",
+              right: bounds.x - padding - 50,
+              top: bounds.y - padding,
+              width: bounds.width + padding * 2,
+              height: bounds.height + padding * 2,
+              borderColor: "red",
+              borderWidth: 4,
+              borderRadius: 3,
+            }}
+          />
+        );
+      })}
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
